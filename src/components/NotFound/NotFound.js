0553590a130b85c7f21b8052c6ab5ca71c9adbe8
herohossen <delowarhossen1224@gmail.com>
2022-04-05 05:32:56 +0600

    NotFound Page Done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { useNavigate } from "react-router-dom";
+import NotFoundImg from "../../404-error-page-not-found.jpg";
+import "./NotFound.css";
 const NotFound = () => {
-    return (
-        <div>
-            404 not found
-        </div>
-    );
+    const navigate = useNavigate();
+  return (
+    <div>
+      <div className="notfound-img">
+        <img src={NotFoundImg} alt="" />
+        <button class="btn-home custom-btn" onClick={() => navigate("/home")}>GO TO HOMEPAGE</button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
